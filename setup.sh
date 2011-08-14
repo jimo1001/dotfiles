d2f9ada0369cff78b3416d74ad940441bd9a2b11
jimo1001 <jimo1001@gmail.com>
2011-08-14 19:53:37 +0900

    add a setup file.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- coding: utf-8; mode: shell-script -*-
+
+SCRIPT_FILE=$0
+DOTFILE_PATH=${SCRIPT_FILE%/*}
+
+ln -sh $DOTFILE_PATH/.emacs.d/ $HOME/.emacs.d
+ln -sh $DOTFILE_PATH/.screen/ $HOME/.screen
+ln -sh $DOTFILE_PATH/.zsh/ $HOME/.zsh
+ln -sh $DOTFILE_PATH/.bash/ $HOME/.bash
+ln -sh $DOTFILE_PATH/.vim/ $HOME/.vim
+ln -s $DOTFILE_PATH/.tmux.conf $HOME/.tmux.conf
+
+pushd $HOME
+ln -s .screen/.screenrc .screenrc
+ln -s .zsh/.zshenv .zshenv
+ln -s .bash/.bashrc .bashrc
+ln -s .vim/.vimrc .vimrc
+popd
+
