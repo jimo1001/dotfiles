cd97e2c07ffd2c2a6c54827c6dbccb57a8aa3a87
jimo1001 <jimo1001@gmail.com>
2011-08-14 20:18:56 +0900

    add a setup file for Windows(Vista/7).

---
+++ setup.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set DOTFILE_PATH=%~dp0
+set CURRENT_PATH=%CD%
+
+mklink /D "%HOME%/.emacs.d" "%DOTFILE_PATH%/.emacs.d"
+mklink /D "%HOME%/.screen" "%DOTFILE_PATH%/.screen"
+mklink /D "%HOME%/.zsh" "%DOTFILE_PATH%/.zsh"
+mklink /D "%HOME%/.bash" "%DOTFILE_PATH%/.bash"
+mklink /D "%HOME%/.vim" "%DOTFILE_PATH%/.vim"
+mklink "%HOME%/.tmux.conf" "%DOTFILE_PATH%/.tmux.conf"
+
+cd "%HOME%"
+mklink .screenrc .screen\.screenrc
+mklink .zshenv .zsh\.zshenv
+mklink .bashrc .bash\.bashrc
+mklink .vimrc .vim\.vimrc
+cd "%CURRENT_PATH%"
--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 SCRIPT_FILE=$0
 DOTFILE_PATH=${SCRIPT_FILE%/*}
 
-ln -sh $DOTFILE_PATH/.emacs.d/ $HOME/.emacs.d
-ln -sh $DOTFILE_PATH/.screen/ $HOME/.screen
-ln -sh $DOTFILE_PATH/.zsh/ $HOME/.zsh
-ln -sh $DOTFILE_PATH/.bash/ $HOME/.bash
-ln -sh $DOTFILE_PATH/.vim/ $HOME/.vim
+ln -sh $DOTFILE_PATH/.emacs.d $HOME/.emacs.d
+ln -sh $DOTFILE_PATH/.screen $HOME/.screen
+ln -sh $DOTFILE_PATH/.zsh $HOME/.zsh
+ln -sh $DOTFILE_PATH/.bash $HOME/.bash
+ln -sh $DOTFILE_PATH/.vim $HOME/.vim
 ln -s $DOTFILE_PATH/.tmux.conf $HOME/.tmux.conf
 
 pushd $HOME
@@ -17,4 +17,3 @@ ln -s .zsh/.zshenv .zshenv
 ln -s .bash/.bashrc .bashrc
 ln -s .vim/.vimrc .vimrc
 popd
-
