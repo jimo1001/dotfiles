9693eeb0241b7351afaf19d5d4dd06c3e56a39df
Yoshinobu Fujimoto <jimo1001@gmail.com>
2016-07-22 01:02:42 +0900

    small fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,13 @@
 ;; -*- coding: utf-8; mode: emacs-lisp; -*-
 ;;; init.el --- Initialization file for GNU Emacs
 
+
+;; load ~/.emacs.d/site-lisp
+(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
 ;; cask
 (when (require 'cask nil t)
   (cask-initialize))
@@ -16,12 +23,6 @@
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   ;; initialzie
   (package-initialize))
-
-;; load ~/.emacs.d/site-lisp
-(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
 
 ;; load init files
 (when (require 'init-loader nil t)
