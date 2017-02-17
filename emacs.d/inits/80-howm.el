ae202e304034d861274bd7758c6bdaa935f153b0
Yoshinobu Fujimoto <jimo1001@gmail.com>
2017-02-17 23:48:17 +0900

    some small fixes

--- emacs.d/inits/80-howm.el
+++ emacs.d/inits/80-howm.el
@@ -6,19 +6,21 @@
 (use-package howm
   :bind ("C-c , ," . howm-menu)
   :init
-  (setq howm-view-title-regexp "^\\(=\\|#\\)\\( +[^-]\\(.*\\)\\|\\)$"
+  (setq howm-excluded-file-regexp "/\\.#\\|[~#]$\\|\\.bak$\\|/CVS/\\|\\.doc$\\|\\.pdf$\\|\\.ppt$\\|\\.xls$"
+        howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.org"
+        howm-history-file (expand-file-name "~/tmp/howm-history")
+        howm-keyword-file (expand-file-name "~/tmp/howm-keys")
+        howm-list-all-title t
+        howm-list-recent-title t
+        howm-menu-expiry-hours 2
+        howm-menu-file (locate-user-emacs-file "menu.howm")
         howm-menu-lang 'en
         howm-menu-name-format "*howm*"
-        howm-view-split-horizontally t
-        howm-list-recent-title t
-        howm-list-all-title t
-        howm-menu-expiry-hours 2
-        howm-menu-file "~/.emacs.d/menu.howm"
-        howm-view-summary-persistent nil
-        howm-menu-schedule-days-before 2
         howm-menu-schedule-days 7
-        howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.org"
-        howm-excluded-file-regexp "/\\.#\\|[~#]$\\|\\.bak$\\|/CVS/\\|\\.doc$\\|\\.pdf$\\|\\.ppt$\\|\\.xls$")
+        howm-menu-schedule-days-before 2
+        howm-view-split-horizontally t
+        howm-view-summary-persistent nil
+        howm-view-title-regexp "^\\(=\\|#\\)\\( +[^-]\\(.*\\)\\|\\)$")
   :config
   (add-to-list 'helm-boring-buffer-regexp-list "\\*howmM:")
   (custom-set-faces
