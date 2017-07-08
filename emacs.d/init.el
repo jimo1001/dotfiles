6d72b1563ef26009bca3f96b1fc15920a3b6b55c
Yoshinobu Fujimoto <jimo1001@gmail.com>
2017-07-08 16:58:21 +0900

    small updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,8 +11,8 @@
 (require 'package)
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t))
 
