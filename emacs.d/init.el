bfe3fa33e952bf1c14d8a314eaa1bda7cc2dd7bf
Yoshinobu Fujimoto <jimo1001@gmail.com>
2018-01-11 14:48:36 +0900

    small fixes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,7 +11,7 @@
 (require 'package)
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t))
