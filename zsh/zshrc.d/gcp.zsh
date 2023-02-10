57e2c91b8ad51277592b6ff873c1f16cd3f81cb3
Yoshinobu Fujimoto <jimo1001@yoshinobunombp.lan>
2023-02-10 13:39:01 +0900

    small fixes

--- zsh/zshrc.d/gcp.zsh
+++ zsh/zshrc.d/gcp.zsh
@@ -1,5 +1,3 @@
-
-
 source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 
