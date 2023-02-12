cbde0c668c98d09875a45047dca379e447a08734
Yoshinobu Fujimoto <jimo1001@gmail.com>
2023-02-12 18:40:52 +0900

    gpg signature

--- zsh/zshrc.d/git.zsh
+++ zsh/zshrc.d/git.zsh
@@ -2,3 +2,6 @@
 alias git-cleanup-branches="git branch -q -l --merged | grep -v '^* ' | grep -v develop | xargs -L1 git branch -d"
 alias git-cleanup-unmerged-branches="git branch -q -l --no-merged | grep -v '^* ' | grep -v develop | xargs -L1 git branch -D"
 
+# GPG Signing
+export GPG_TTY=$(tty)
+
